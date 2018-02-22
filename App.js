8dd0269f9493ee40a06e7b8e35db3b3f3387f3b3
Vanskins <vansabacajan@gmail.com>
2018-02-22 12:08:06 +0800

    update

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, FlatList, TouchableOpacity, Dimensions, Alert } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList, TouchableOpacity, Dimensions, Alert, Image} from 'react-native';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 
@@ -9,6 +9,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ecf0f1',
+    alignItems: 'center',
   },
   button: {
     width: PAGE.width,
@@ -28,47 +29,74 @@ export default class App extends React.Component {
     super();
     this.state = {
       text: '',
-      todos: [
-        {
-          todo: 'Wash dishes',
-          done: false,
-        },
-        {
-          todo: 'Do my Capstone project',
-          done: false,
-        },
-        {
-          todo: 'Study Programming',
-          done: true,
-        },
-      ],
+      todos: [],
     }
   }
 
-  _renderItem = ({ item }) => {
-    return (
+  todoList = () => {
+    return this.state.todos.map( (item, index) => 
       <TouchableOpacity
-        style={{ height: 40, justifyContent: 'center', borderWidth: 1, marginTop: 15, backgroundColor: item.done ? 'green' : 'white', color: item.done ? 'white' : 'black' }}
+        key ={index}
+        style={{ width: Dimensions.get('window').width * 0.9 ,height: 40, justifyContent: 'center', borderWidth: 1, marginTop: 15, backgroundColor: item.done ? 'white' : 'white', borderRadius: 15 }}
         onPress={() => {
+          const label = item.done ? 'UNDONE': 'DONE';
           Alert.alert(
-            'Todo',
-            item.todo,
+            'Alert Title',
+            'My Alert Msg',
             [
-              { text: 'DONE', onPress: () => console.log('Ask me later pressed') },
-              { text: 'DELETE', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+              {
+                text: label, onPress: () => {
+                  if(label === 'DONE'){
+                    let todoCopy = this.state.todos;
+                    todoCopy[index].done = true;
+                    this.setState({
+                      todos: todoCopy,
+                    })
+                  } else {
+                    let todoCopy = this.state.todos;
+                    todoCopy[index].done = false;
+                    this.setState({
+                      todos: todoCopy,
+                    })
+                  }
+              } },
+              { text: 'DELETE', onPress: () => {
+                const todoIndex = this.state.todos.findIndex(a => a.todo === item.todo);
+                let todoCopy = this.state.todos;
+                todoCopy.splice(todoIndex,1);
+                this.setState({
+                  todos: todoCopy,
+                })
+              } },
             ],
             { cancelable: false }
           )
         }}
-      >
-        <Text style={{ alignSelf: 'center' }}>{item.todo}</Text>
-      </TouchableOpacity>
-    );
-  };
 
-  _add = () => {
-    const { text, todos } = this.state;
-    todos.push({todo: text, done: false });
+      >
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: Dimensions.get('window').width * 0.9}}>
+          <View>
+            <Text style={{ color: 'black', paddingLeft: 20, height: 25 }}>{item.todo}</Text>
+          </View>
+          <View>
+          {
+            item.done && 
+              <Image
+                style={{ width: 25, height: 25, marginRight: 15 }}
+                source={require('./images/check.png')}
+              />
+          }
+          </View>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  submit = () => {
+    this.setState({
+      todos: [...this.state.todos, { todo: this.state.text, done: false }]
+    })
+    this.textInput.clear(); 
   }
 
   render() {
@@ -78,21 +106,25 @@ export default class App extends React.Component {
         <Header />
         <View style={[styles.container]}>
           <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            ref={input => { this.textInput = input }}
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: Dimensions.get('window').width }}
             onChangeText={(text) => this.setState({ text })}
+            onSubmitEditing={() => {
+              this.submit();
+            } }
             value={this.state.text}
             placeholder="What will you do today"
           />
           <TouchableOpacity
             style={[styles.button]}
-            onPress={this._add()}
+            onPress={()=>{
+              this.submit();
+              this.textInput.clear();
+            }}
           >
             <Text style={[styles.text]}>Submit Todo</Text>
           </TouchableOpacity>
-          <FlatList
-            data={todos}
-            renderItem={this._renderItem}
-          />
+          {this.todoList()}
         </View>
         <Footer />
       </View>
